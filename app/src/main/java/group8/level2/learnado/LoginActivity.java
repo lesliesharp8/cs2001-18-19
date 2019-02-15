203560296174bd7978b9d96f44e86d5b6ac06d7a
AkashB <1707247@brunel.ac.uk>
2019-02-15 22:35:55 +0000

    Sign up and Login Finished. Can improve later on.

--- app/src/main/java/group8/level2/learnado/LoginActivity.java
+++ app/src/main/java/group8/level2/learnado/LoginActivity.java
@@ -2,12 +2,73 @@ package group8.level2.learnado;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    private EditText Name;
+    private EditText Password;
+    private Button Login;
+    private int counter = 5;
+    private TextView Register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        Name =(EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Login=(Button)findViewById(R.id.btnLogIn);
+        Register= (TextView)findViewById(R.id.tvRegister);
+
+
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
+    private void validate(String userName, String userPassword){
+
+        if(userName.equals("Admin") && userPassword.equals("1234")){
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }else{
+
+            counter --;
+
+            if(counter == 0){
+                Login.setEnabled(false);
+
+            }
+
+
+        }
+    }
+
+
+
+
 }
