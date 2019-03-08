98ac2106974c74cc0aa4e8e41f5555dbca709d0b
AkashB <1707247@brunel.ac.uk>
2019-03-08 18:04:48 +0000

    back button on register page fully working, enables the user to go back to the login page if they accidentally pressed on register textview.

--- app/src/main/java/group8/level2/learnado/SignUp.java
+++ app/src/main/java/group8/level2/learnado/SignUp.java
@@ -1,15 +1,31 @@
 package group8.level2.learnado;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SignUp extends AppCompatActivity {
+    private ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        back = (ImageView)findViewById(R.id.Back);
+
+       back.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(SignUp.this, LoginActivity.class);
+               startActivity(intent);
+           }
+       });
   //needs to be connected to the database
     }
+
+
 
 }
