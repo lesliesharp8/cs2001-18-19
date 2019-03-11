ec934ce0ee6942ddd88df47e18321f492cfc937a
leslie sharp <33091604+lesliesharp8@users.noreply.github.com>
2019-03-11 19:52:37 +0000

    Resolved OnClick View Error
    I have resolved the error where the system was not connect my button to the view method which tells the app to go to the main menu i.e. main activity

--- SettingsActivity.java
+++ SettingsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -11,8 +12,16 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        }
-            public void onClick(View onClick) {
+        Button next = findViewById(R.id.button5);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), SettingsActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+    }
+
+            public void View(View view) {
                 Intent i = new Intent(this, MainActivity.class);
                 startActivity(i);
     }
