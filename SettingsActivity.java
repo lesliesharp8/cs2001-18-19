6141ee160089092857804939165a316df94d15ff
leslie sharp <33091604+lesliesharp8@users.noreply.github.com>
2019-03-11 19:12:25 +0000

    Connection of pages
    Manual commit due to device issues but no errors at the moment.

---
+++ SettingsActivity.java
@@ -0,0 +1,20 @@
+package group8.level2.learnado;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        getActionBar().show();
+        }
+            public void onClick(View onClick) {
+                Intent i = new Intent(this, MainActivity.class);
+                startActivity(i);
+    }
+}
