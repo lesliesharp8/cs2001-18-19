df46c273ac30741c18a7ebaf1f57dc410fa9710b
ThomasHunt96 <44229633+ThomasHunt96@users.noreply.github.com>
2019-03-12 15:49:32 +0000

    Updated master branch with Leslies manually uploaded code

--- app/src/main/java/group8/level2/learnado/SettingsActivity.java
+++ app/src/main/java/group8/level2/learnado/SettingsActivity.java
@@ -1,7 +1,10 @@
 package group8.level2.learnado;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Button next = findViewById(R.id.button5);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), SettingsActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+    }
+
+    public void View(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
