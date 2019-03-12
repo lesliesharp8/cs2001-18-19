3c9cbe90612a56084d2a99d67f9d14f11b8d5455
ThomasHunt96 <44229633+ThomasHunt96@users.noreply.github.com>
2019-03-12 16:26:11 +0000

    Updated master branch with Leslies manually uploaded code

--- app/src/main/java/group8/level2/learnado/MainActivity.java
+++ app/src/main/java/group8/level2/learnado/MainActivity.java
@@ -1,10 +1,9 @@
 package group8.level2.learnado;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+
+import android.widget.FrameLayout;
 import android.support.design.widget.NavigationView;
+import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,11 +11,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
