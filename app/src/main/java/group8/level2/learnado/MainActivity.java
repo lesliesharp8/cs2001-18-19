b78b1c4add53b85801940b727135f89fa1c9c44d
lesliesharp <1707077@brunel.ac.uk>
2019-03-12 15:59:00 +0000

    Correcting my commit mistakes due to using the wrong version of the application

--- app/src/main/java/group8/level2/learnado/MainActivity.java
+++ app/src/main/java/group8/level2/learnado/MainActivity.java
@@ -1,7 +1,10 @@
 package group8.level2.learnado;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button next = findViewById(R.id.button6);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        }) ;
     }
 }
