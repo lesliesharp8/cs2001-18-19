02e87ca0717d8fac6282c41bb8287f46a24926d4
lesliesharp <1707077@brunel.ac.uk>
2019-03-20 15:41:54 +0000

    Placeholder button functionality added to the change password and email buttons.

--- app/src/main/java/group8/level2/learnado/SettingsActivity.java
+++ app/src/main/java/group8/level2/learnado/SettingsActivity.java
@@ -1,26 +1,93 @@
 package group8.level2.learnado;
-
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
-
+    private String pwChange = "";
+    private String emailChange = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().setTitle("Book");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_settings);
-        Button next = findViewById(R.id.button6);
+        Button next = findViewById(R.id.button5);
         next.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent myIntent = new Intent(view.getContext(), SettingsActivity.class);
                 startActivityForResult(myIntent, 0);
             }
         });
+
+        final Button changePassword = findViewById(R.id.button);
+        changePassword.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+                builder.setTitle("Change Password");
+
+                final EditText input = new EditText(SettingsActivity.this);
+
+                input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                builder.setView(input);
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        pwChange = input.getText().toString();
+
+                        Context context = getApplicationContext();
+                        CharSequence text = pwChange;
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+
+
+                    }
+                });
+                builder.show();
+
+            }
+        });
+        final Button changeEmail= findViewById(R.id.button2);
+        changeEmail.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+                builder.setTitle("Change Email");
+
+                final EditText input = new EditText(SettingsActivity.this);
+
+                input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+                builder.setView(input);
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        emailChange = input.getText().toString();
+
+                        Context context = getApplicationContext();
+                        CharSequence text = emailChange;
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+
+                    }
+                });
+                builder.show();
+
+            }
+        });
+
     }
 
     public void View(View view) {
